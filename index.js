7fffb25c444edaa04b0976883cfd676ba7564012
Nicole Mansour <nmansour1995@gmail.com>
2018-06-28 17:13:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,17 @@ const logWhisper = function(string) {
   console.log("hello")
 }
 
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+  
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
 
