f3ed098dcfc56e23c7c5d7608ee2ff571768487d
Nicole Mansour <nmansour1995@gmail.com>
2018-06-28 17:12:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+const logShout = function(string) {
+  console.log ("HELLO")
+}
+
+const logWhisper = function(string) {
+  console.log("hello")
+}
+
+
